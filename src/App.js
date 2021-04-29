bd3d1eb7b20a63645845f46b9fa1287961c04f9f
Cosmas <24183871+Cosmas6@users.noreply.github.com>
2021-04-29 17:15:20 +0300

    Carousel Changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from "react";
-
-
 //GlobalStyle
 import GlobalStyle from "./components/GlobalStyle";
 import styled from "styled-components";
--- src/components/DesignSection.js
+++ src/components/DesignSection.js
@@ -6,32 +6,97 @@ import PlotR79 from "./ImageSliders/PlotR79";
 import PlotR185 from "./ImageSliders/PlotR185";
 import designHero from "../img/designHero.jpeg";
 import styled from "styled-components";
+import plot1 from "../img/D68/1.jpg";
+import plot2 from "../img/D68/2.jpg";
+import plot3 from "../img/D68/3.jpg";
 
 const Designs = () => {
   return (
-    <DesignPage>
-      <div className="title">
-        <h1>DESIGNS</h1>
-      </div>
+    <>
+      <DesignPage>
+        <div className="title">
+          <h1>DESIGNS</h1>
+        </div>
 
-      <div className="slideshow">
-        <h3>Plot No. D68</h3>
-        <PlotD68 />
-        <h3>Plot No. D118</h3>
-        <PlotD118 />
-        <h3>Maisonette</h3>
-        <Maisonette />
-        <h3>Plot No. R79</h3>
-        <PlotR79 />
-        <h3>Plot No. R185</h3>
-        <PlotR185 />
-      </div>
-      <div className="belowtext">
-        
-      </div>
-    </DesignPage>
-  );
-};
+        <div className="slideshow">
+          <h3>Plot No. D68</h3>
+          <PlotD68 />
+          <h3>Plot No. D118</h3>
+          <PlotD118 />
+          <h3>Maisonette</h3>
+          <Maisonette />
+          <h3>Plot No. R79</h3>
+          <PlotR79 />
+          <h3>Plot No. R185</h3>
+          <PlotR185 />
+        </div>
+        <div className="belowtext"></div>
+      </DesignPage>
+	<div class="container">
+		<div class="carousel slide" id="main-carousel" data-ride="carousel">
+			<ol class="carousel-indicators">
+				<li data-target="#main-carousel" data-slide-to="0" class="active"></li>
+				<li data-target="#main-carousel" data-slide-to="1"></li>
+				<li data-target="#main-carousel" data-slide-to="2"></li>
+				<li data-target="#main-carousel" data-slide-to="3"></li>
+			</ol>
+
+      {/* <!-- /.carousel-indicators --> */}
+			
+			<div class="carousel-inner">
+				<div class="carousel-item active">
+					<img class="d-block img-fluid" src="https://s19.postimg.cc/qzj5uncgj/slide1.jpg" alt=""/>
+					<div class="carousel-caption d-none d-md-block">
+						<h1>Mountain</h1>
+					</div>
+				</div>
+				<div class="carousel-item">
+					<img class="d-block img-fluid" src="https://s19.postimg.cc/lmubh3h0j/slide2.jpg" alt=""/>
+					<div class="carousel-caption d-none d-md-block">
+						<h3>Mountain</h3>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci esse vitae exercitationem fugit, numquam minus!</p>
+					</div>
+				</div>
+				<div class="carousel-item">
+					<img class="d-block img-fluid" src="https://s19.postimg.cc/99hh9lr5v/slide3.jpg" alt=""/>
+					<div class="carousel-caption d-none d-md-block">
+						<h3>Mountain</h3>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci esse vitae exercitationem fugit, numquam minus!</p>
+					</div>
+				</div>
+				<div class="carousel-item">
+					<img src="https://s19.postimg.cc/nenabzsnn/slide4.jpg" alt="" class="d-block img-fluid"/>
+					<div class="carousel-caption d-none d-md-block">
+						<h3>Mountain</h3>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci esse vitae exercitationem fugit, numquam minus!</p>
+					</div>
+				</div>
+        </div>
+        {/* <!-- /.carousel-inner --> */}
+			
+			<a href="#main-carousel" class="carousel-control-prev" data-slide="prev">
+				<span class="carousel-control-prev-icon"></span>
+				<span class="sr-only" aria-hidden="true">Prev</span>
+			</a>
+			<a href="#main-carousel" class="carousel-control-next" data-slide="next">
+				<span class="carousel-control-next-icon"></span>
+				<span class="sr-only" aria-hidden="true">Next</span>
+			</a>
+		</div>
+    {/* <!-- /.carousel --> */}
+	</div>
+  {/* <!-- /.container --> */}
+
+      <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</h1>
+      <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</h1>
+      <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</h1>
+      <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</h1>
+      <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</h1>
+      <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</h1>
+    </>
+    );
+  };
+
 
 const DesignPage = styled.div`
   scroll-behavior: smooth;
